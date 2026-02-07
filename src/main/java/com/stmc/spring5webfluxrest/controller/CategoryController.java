3f37b5878806e303454b1eb58cb65332fd651712
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-02-07 01:17:35 -0500

    Section 32 - Class 456: Create Category Controller

---
+++ src/main/java/com/stmc/spring5webfluxrest/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package com.stmc.spring5webfluxrest.controller;
+
+import com.stmc.spring5webfluxrest.model.Category;
+import com.stmc.spring5webfluxrest.repository.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by: GvN
+ * Date: 06-Feb-26
+ * Time: 23:10
+ * Project Name: spring5-webflux-rest
+ */
+@RestController
+public class CategoryController {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories")
+    Flux<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    Mono<Category> findById(@PathVariable String id) {
+        return categoryRepository.findById(id);
+    }
+}
